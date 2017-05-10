10ac21c96aec91958b5c8d7e1b9b462057c4c09a
Jeremy Lincoln <bossco28@gmail.com>
2017-05-10 14:34:04 -0400

    Constructors

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(15, 101);
+
+            Console.WriteLine($"emp years of service: {emp.YearsOfService}");
+            Console.WriteLine($"emp office: {emp.Office}");
+
+            Employee emp2 = new Employee();
+            emp2.YearsOfService = 5;
+            emp2.Office = 102;
+
+            Console.WriteLine($"emp years of service: {emp2.YearsOfService}");
+            Console.WriteLine($"emp office: {emp2.Office}");
+
+            //Working with the default along with some initialization.
+            Employee emp3 = new Employee()
+            {
+                YearsOfService = 20,
+                Office = 301
+            };
+
+            Console.WriteLine($"emp years of service: {emp3.YearsOfService}");
+            Console.WriteLine($"emp office: {emp3.Office}");
+
+        }
+    }
+}
