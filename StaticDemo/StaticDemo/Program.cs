201b54b1872e0eac11b7da65463ae5e72d6786e6
Jeremy Lincoln <bossco28@gmail.com>
2017-05-10 14:57:04 -0400

    Static Method Classes

---
+++ StaticDemo/StaticDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection = string.Empty;
+            while (selection != "q" && selection != "Q")
+            {
+                Console.Write("Enter C)elsius to Fahrenheit or F)ahrenheit to Celsius or Q)uit: ");
+                selection = Console.ReadLine();
+                double farenheit = 0;
+                double celsius = 0;
+
+                switch (selection)
+                {
+                    case "C":
+                    case "c":
+                        Console.WriteLine("Please enter the Celsius temperature: ");
+                       
+                        farenheit = TempConverter.CtoF(Console.ReadLine());
+                        Console.WriteLine($"Temperature in f is {farenheit:f2}");
+                        break;
+                    case "F":
+                    case "f":
+                        Console.WriteLine("Please enter the Farenheit temperature: ");
+                        
+                        celsius = TempConverter.FtoC(Console.ReadLine());
+                        Console.WriteLine($"Temperature in c is {celsius:f2}");
+                        break;
+                    case "Q":
+                    case "q":
+                        break;
+                    default:
+                        Console.WriteLine("Please Try again");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StaticDemo/StaticDemo/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    class TempConverter
+    {
+        public static double CtoF(string tempC)
+        {
+            double c = Double.Parse(tempC);
+            double f = (c * 9 / 5) + 32;
+            return f;
+        }
+
+        public static double FtoC(string tempF)
+        {
+            double f = Double.Parse(tempF);
+            double c = (f - 32) *  5/9;
+            return c;
+        }
+    }
+}
